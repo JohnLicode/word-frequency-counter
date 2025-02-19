8c0b336cd06fafb13fd02ac9b6cf8ebbf27a38d6
Akamatsuki <joshuaakamatsu18@google.com>
2025-02-19 17:30:42 +0800

    Uploaded Part1 Practice3

--- Part1_Prac1.php
+++ Part1_Prac1.php
@@ -18,15 +18,12 @@
         $b = floatval($_POST['side2']);
         $c = floatval($_POST['side3']);
 
-        // Check if the sides form a valid triangle
         if ($a + $b > $c && $a + $c > $b && $b + $c > $a) {
-            // Calculate semi-perimeter
+
             $s = ($a + $b + $c) / 2;
 
-            // Calculate area using Heron's formula (without sqrt function)
             $area = ($s * ($s - $a) * ($s - $b) * ($s - $c)) ** 0.5;
 
-            // Display the formatted result
             echo "<p>Area of the triangle: " . number_format($area, 2) . "</p>";
         } else {
             echo "<p>Invalid triangle sides. Please enter valid values.</p>";
---
+++ Part1_Prac3.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$matrix = [
+    [12, 23, 34],
+    [45, 55, 62],
+    [71, 84, 90]
+];
+
+$i = 0;
+while ($i < count($matrix)) {
+    $j = 0;
+    while ($j < count($matrix[$i])) {
+        if ($matrix[$i][$j] % 2 == 0) {
+            echo $matrix[$i][$j] . " ";
+        }
+        $j++;
+    }
+    $i++;
+}
+?>
